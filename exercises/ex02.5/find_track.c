02fe6ba92223360d54281f068bdc8bdfe32f0200
Gracey Wilson <gracey.wilson@students.olin.edu>
2020-02-13 21:48:03 -0500

    find_track.c done!

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -2,11 +2,14 @@
 
 Modified version of an example from Chapter 2.5 of Head First C.
 
+Author: Gracey Wilson
+Note: I had a few questions answered by NINJA Prava, and worked with William Derksen and Shirin Kuppusamy for portions of this HW!
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -20,7 +23,6 @@ char tracks[][80] = {
 
 
 // Finds all tracks that contain the given string.
-//
 // Prints track number and title.
 void find_track(char search_for[])
 {
@@ -32,12 +34,36 @@ void find_track(char search_for[])
     }
 }
 
+// Determines whether a string matches a pattern according to the definition of regular expressions.
+// Returns 0 if there's an error (if the string is not contained in the pattern) and return 1 if the pattern does exist.
+int match(const char *string, char *pattern)
+{
+    int status;
+    regex_t re;
+
+    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) {
+      printf("Did not compile - there was an error\n"); // I imagine I could use regerror() here but I couldn't figure it out in the documentation. Is that a requirement?
+      return(0);      /* report error */
+    }
+    status = regexec(&re, string, (size_t) 0, NULL, 0);
+    regfree(&re);
+    if (status != 0) {
+      printf("Did not compile - there was an error\n"); // I imagine I could use regerror() here but I couldn't figure it out in the documentation. Is that a requirement?
+      return(0);      /* report error */
+    }
+    return(1);
+}
+
 // Finds all tracks that match the given pattern.
-//
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+  int i;
+  for (i=0; i<NUM_TRACKS; i++) {
+      if ((match(tracks[i], pattern)) == 1) {     // if match() returns 1 for that line,
+          printf("Track %i: '%s'\n", i, tracks[i]); //print that line
+      }
+  }
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -58,8 +84,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    // find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -1,3 +1,22 @@
 /*
-Still working on this one - will push very soon! Just wanted to make sure you had the directory link.
+Gracey Wilson
+probably want to use fgets, strlen and atoi
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int convert(int *s) {
+  int num = atoi(s);
+  return 0;
+}
+
+int main() {
+  // prompt
+  //make a pointer to the string user inputted
+  // convert(int *s);
+  // add them all up
+  // error if sum exceeds largest int that can be repped
+  return 0;
+}
